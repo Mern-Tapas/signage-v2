30b2d6c3b4a515e76b6669565fbf31e869e80ea3
Mern-Tapas <cyb6261452510@gmail.com>
2025-12-21 00:01:01 +0530

    udpate

--- components/ui/pages/UserPlaylist.tsx
+++ components/ui/pages/UserPlaylist.tsx
@@ -2,11 +2,12 @@
 import React from 'react'
 import Container from '@/components/layout/Container'
 import { Typography } from '@/components/typography/typography'
-import { Clock, EllipsisVertical, Monitor, PlusIcon, Upload, Video, } from 'lucide-react'
+import { Clock, EllipsisVertical, ListVideo, Monitor, PlusIcon, Upload, Video, } from 'lucide-react'
 import DeviceList from '../custome/DeviceList'
 import { Card, CardHeader } from '../custome/Card'
 import { Button } from '../custome/Button'
 import CardStack from '../custome/CardStack'
+import { Dialog, DialogBody, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '../custome/Dialog'
 
 function UserPlaylist() {
     return (
@@ -24,16 +25,22 @@ function UserPlaylist() {
                 {/* Playlist Details */}
                 <Container padding='lg' radius='xl' variant='primary' className=''>
 
-                    <div className='flex justify-between'>
+                    <div className='flex lg:flex-row flex-col justify-between gap-4 lg:items-center '>
 
-                        <Container className='flex flex-col gap-4 shrink-0 '>
+                        <Container className='flex gap-4  '>
+                            <div className='flex bg-pink-100 rounded-lg p-2 h-14 w-14 shrink-0'>
+
+                                <ListVideo className='m-auto text-pink-600' size={24} />
+
+                            </div>
                             <div className="max-w-180">
                                 <Typography variant='h4' weight='bold'>Phoenix Creatives Advertisment Power point</Typography>
                                 <Typography variant='body2' color='muted'>Connect playlist and manage your ads</Typography>
                             </div>
 
                         </Container>
-                        <Container radius='md' className='bg-gray-100 w-full  border-gray-300' variant='primary'>
+
+                        <Container radius='md' className='bg-gray-100 border-gray-300' variant='primary'>
                             <div className="flex gap-2">
                                 <Button size='md' variant='outline' icon={<Upload size={16} />}>Assign</Button>
                                 <Button size='md' variant='outline' >Preview</Button>
@@ -41,6 +48,7 @@ function UserPlaylist() {
                                 <Button size='icon' variant='outline' icon={<EllipsisVertical size={16} />}> </Button>
                             </div>
                         </Container>
+
                     </div>
 
 
@@ -61,8 +69,50 @@ function UserPlaylist() {
                             {/* <Typography variant='body2' color='muted'>Playlist Items</Typography> */}
                         </div>
                         <Container className='flex items-center'>
-                            <Button variant="primary" className='shadow-md md:flex hidden' size='md' icon={<PlusIcon size={18} strokeWidth={1.5} />}>Add Content</Button>
-                            <Button variant="primary" className='shadow-md md:hidden flex' size='icon' icon={<PlusIcon size={18} strokeWidth={1.5} />}></Button>
+
+
+
+                            <Dialog>
+                                <DialogTrigger>
+                                    <Button variant="primary" className='shadow-md md:flex hidden' size='md' icon={<PlusIcon size={18} strokeWidth={1.5} />}>Add Content</Button>
+                                    <Button variant="primary" className='shadow-md md:hidden flex' size='icon' icon={<PlusIcon size={18} strokeWidth={1.5} />}></Button>
+                                </DialogTrigger>
+                                <DialogContent>
+                                    <DialogHeader>
+                                        <DialogTitle>Create User</DialogTitle>
+                                        <DialogDescription>
+                                            Fill in the details below to create a new user.
+                                        </DialogDescription>
+                                    </DialogHeader>
+
+                                    <DialogBody>
+                                        <form className="space-y-4">
+                                            <div>
+                                                <Typography variant='body2' className="">Name</Typography >
+                                                <input
+                                                    type="text"
+                                                    className="mt-1 block w-full rounded-md border border-gray-300 dark:border-gray-700 bg-transparent px-3 py-2"
+                                                />
+                                            </div>
+                                            <div>
+                                                <Typography variant='body2' className="">Email</Typography>
+                                                <input
+                                                    type="email"
+                                                    className="mt-1 block w-full rounded-md border border-gray-300 dark:border-gray-700 bg-transparent px-3 py-2"
+                                                />
+                                            </div>
+                                        </form>
+                                    </DialogBody>
+
+                                    <DialogFooter>
+                                        <DialogClose>Cancel</DialogClose>
+                                        <Button variant="primary">Save</Button>
+                                    </DialogFooter>
+                                </DialogContent>
+                            </Dialog>
+
+
+
                         </Container>
                     </Container>
                 </CardHeader>
